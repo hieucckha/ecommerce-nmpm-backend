b593136a31b1dd1a8e915cf05b787ec76e24cc97
hieucckha <conpasslaconheo@gmail.com>
2022-12-20 10:08:27 +0700

    Test with vercel

--- src/app/index.js
+++ src/app/index.js
@@ -8,13 +8,13 @@ app.use(require('./middleware.app'));
 // route
 app.get('/', (req, res, next) => {
   res.status(200).json({
-    status: "Success",
-    message: "This is from index base :p"
-  })
-})
-app.use('/api/v1', require('../api/v1'));
+    status: 'Success',
+    message: 'This is from index base :p',
+  });
+});
+// app.use('/api/v1', require('../api/v1'));
 
 // catch error
-app.use(require('./error.app'));
+// app.use(require('./error.app'));
 
 module.exports = app;
